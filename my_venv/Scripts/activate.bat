2b8a151df26d1b2496b32a0a0ed32b2189719360
Kristen Rutherford <kmr4@rice.edu>
2018-09-16 17:06:03 -0500

    Initial commit

---
+++ my_venv/Scripts/activate.bat
@@ -0,0 +1,30 @@
+@echo off
+set "VIRTUAL_ENV=C:\Users\Owner\DOCUME~1\CSCIE-~1\Lab2\lab2\my_venv"
+
+if defined _OLD_VIRTUAL_PROMPT (
+    set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
+) else (
+    if not defined PROMPT (
+        set "PROMPT=$P$G"
+    )
+    set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
+)
+set "PROMPT=(my_venv) %PROMPT%"
+
+REM Don't use () to avoid problems with them in %PATH%
+if defined _OLD_VIRTUAL_PYTHONHOME goto ENDIFVHOME
+    set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
+:ENDIFVHOME
+
+set PYTHONHOME=
+
+REM if defined _OLD_VIRTUAL_PATH (
+if not defined _OLD_VIRTUAL_PATH goto ENDIFVPATH1
+    set "PATH=%_OLD_VIRTUAL_PATH%"
+:ENDIFVPATH1
+REM ) else (
+if defined _OLD_VIRTUAL_PATH goto ENDIFVPATH2
+    set "_OLD_VIRTUAL_PATH=%PATH%"
+:ENDIFVPATH2
+
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
